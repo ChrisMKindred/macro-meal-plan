528426dabfb44be25c3da8a7e15bbd7d2a964174
Chris Kindred <CMKindred@gmail.com>
2020-01-29 07:38:02 -0500

    Updates Recipe table size

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <table class="table">
+            <table class="table table-sm">
                 <thead>
                     <tr>
                         <th scope="col">Item</th>
