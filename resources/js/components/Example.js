c3f4104c0275a7936c868c27be65e5d6c36a8c99
Chris Kindred <cmkindred@gmail.com>
2020-04-19 01:29:42 -0400

    Ajax call to get products.

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 
 function Example() {
     return (
         <FullCalendar
             defaultView="dayGridMonth"
-            plugins={[ dayGridPlugin ]}
+            header={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+              }}
+            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
             />
     );
 }
--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center mb-4">
         <div class="col-md-8">
             <h1>Find</h1>
-            <form name="fatsecret" action="{!! action('FatSecretController@getFood') !!}" method="post">
+            <form name="fatsecret" id="fatsecret" action="{!! action('FatSecretController@getFood') !!}" method="post">
                 @csrf
                 <div class="row">
                     <div class="col">
@@ -43,4 +43,73 @@
         </div>
     </div>
 </div>
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="list-group" id="list">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+const foods = [];
+function runSearch(e){
+    e.preventDefault();
+    foods.length = 0;
+    fetch(e.target.action, {
+        method: 'post',
+        body: new FormData( e.target )
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        if(data.foods) {
+            foods.push(...data.foods.food);
+            console.log(foods);
+            const html = foods.map(food => {
+                var title = ( food.brand_name ) ? food.brand_name + ' : ' + food.food_name : food.food_name;
+                return `
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <h5>${title}</h5>
+                    <small>${food.food_description}</small>
+                </a>
+                `;
+            }).join('');
+            $('#list').html( html );
+            $('.modal-title').html( 'Results For ' + $('#value').val()  );
+           $('.modal').modal('toggle');
+        } else {
+            if ( data.food.food_name ){
+                $("#title").val( ( data.food.brand_name ) ? data.food.brand_name + ' : ' + data.food.food_name : data.food.food_name );
+                $('#calories').val( data.food.servings.serving.calories );
+                $('#fat').val( data.food.servings.serving.fat );
+                $('#carbohydrate').val( data.food.servings.serving.carbohydrate );
+                $('#sugar').val( data.food.servings.serving.sugar );
+                $('#protein').val( data.food.servings.serving.protein );
+            }
+        }
+    });
+}
+
+const searchForm = document.querySelector('#fatsecret');
+const modal = document.querySelector('.modal');
+
+searchForm.addEventListener('submit', runSearch);
+
+
+</script>
 @endsection
